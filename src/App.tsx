d0f191c42331dafdd6790e2a66561bdb72bb3377
peoul <lpeou2@jh.edu>
2025-07-31 00:15:57 -0700

    style - added styling for welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import './App.css'
-import { db } from './db';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./db";
 
 function App() {
-  
+  const [isDark, setIsDark] = useState(true);
 
-  return (
-    <>
-      
-    </>
-  )
+  const isDarkHandler = () => {
+    setIsDark((prev) => !prev);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDark ? "dark" : "light"
+    );
+  }, [isDark]);
+
+return (
+  <>
+    <div className="header">
+      <h1>Streak</h1>
+      <p>Commit Your Habits Like Your Code</p>
+      <hr/>
+    </div>
+    <button onClick={isDarkHandler}>{isDark ? "☀️" : "🌙"}</button>
+  </>
+);
 }
 
-export default App
+export default App;
